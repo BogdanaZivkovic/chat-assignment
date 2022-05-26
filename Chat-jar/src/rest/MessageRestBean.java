464fb5fda6ee5ef6cc02e56b456455bfcc92a3a7
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2022-05-27 01:29:04 +0200

    proba

--- Chat-jar/src/rest/MessageRestBean.java
+++ Chat-jar/src/rest/MessageRestBean.java
@@ -24,29 +24,11 @@ public class MessageRestBean implements MessageRest{
 	private ChatManagerRemote chatManager;
 	
 	@Override
-	public void sendMessageToAll(Message message) {		
-		
-		for (User loggedInUser : chatManager.loggedInUsers()) {	
-			String hostAlias = chatManager.findByUsername(loggedInUser.getUsername()).getHost().getAlias();
-			if(hostAlias.equals(System.getProperty("jboss.node.name") + ":8080")) {
-				AgentMessage agentMessage = new AgentMessage();
-				agentMessage.userArgs.put("receiver", loggedInUser.getUsername());
-				agentMessage.userArgs.put("sender", message.getSender().getUsername());
-				agentMessage.userArgs.put("subject", message.getSubject());
-				agentMessage.userArgs.put("content", message.getContent());	
-				agentMessage.userArgs.put("command", "MESSAGE");
-				messageManager.post(agentMessage);				
-			}
-			else {
-				System.out.println("Host " + hostAlias + " receives a message to distribute");
-				ResteasyClient resteasyClient = new ResteasyClientBuilder().build();
-				ResteasyWebTarget rtarget = resteasyClient.target("http://" + hostAlias + "/Chat-war/api/messages");
-				MessageRest rest = rtarget.proxy(MessageRest.class);
-				rest.sendMessageToUser(message);
-				resteasyClient.close();
-			}
-		}
-		
+	public void sendMessageToAll(Message message) {			
+		for(User user : chatManager.loggedInUsers()) {
+			message.setReceiver(user);
+			sendMessageToUser(message);
+		}	
 	}
 
 	@Override
